a25d0872d30d2787c0e5edba9c036e1e21cb91e3
Jake <2bjake@users.noreply.github.com>
2022-12-01 07:41:43 -0600

    day 1 done

--- Sources/Day1/Day1.swift
+++ Sources/Day1/Day1.swift
@@ -1,8 +1,15 @@
-import Algorithms
+import AdventUtilities
+
+func calorieSums() -> [Int] {
+  input.split(separator: "\n\n").map {
+    $0.split(separator: "\n").compactMap(Int.init).reduce(0, +)
+  }
+}
 
 public func partOne() {
+  print(calorieSums().max()!) // 71471
 }
 
 public func partTwo() {
-
+  print(calorieSums().max(count: 3).reduce(0, +)) // 211189
 }
