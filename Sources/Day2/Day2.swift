e47ec56483acb0db55f3f2d884a4f8d4237a0c39
Jake <2bjake@users.noreply.github.com>
2022-12-03 04:23:41 -0600

    days 2 & 3 done

--- Sources/Day2/Day2.swift
+++ Sources/Day2/Day2.swift
@@ -1,9 +1,68 @@
 import Algorithms
+import AdventUtilities
+
+let partOneGameScores = [
+  "A X": 1 + 3,
+  "A Y": 2 + 6,
+  "A Z": 3 + 0,
+  "B X": 1 + 0,
+  "B Y": 2 + 3,
+  "B Z": 3 + 6,
+  "C X": 1 + 6,
+  "C Y": 2 + 0,
+  "C Z": 3 + 3
+]
 
 public func partOne() {
+  let score = input.split(separator: "\n").map(String.init).compactMap(partOneGameScores).reduce(0, +)
+  print(score)
+}
 
+enum Outcome: Character {
+  case lose = "X"
+  case draw = "Y"
+  case win = "Z"
+
+  var score: Int {
+    switch self {
+      case .lose: return 0
+      case .draw: return 3
+      case .win: return 6
+    }
+  }
+}
+
+enum Play: Character {
+  case rock = "A"
+  case paper = "B"
+  case scissors = "C"
+
+  var score: Int {
+    switch self {
+      case .rock: return 1
+      case .paper: return 2
+      case .scissors: return 3
+    }
+  }
+
+  func playForOutcome(_ outcome: Outcome) -> Play {
+    switch (self, outcome) {
+      case (.rock, .lose): return .scissors
+      case (.rock, .win): return .paper
+      case (.paper, .lose): return .rock
+      case (.paper, .win): return .scissors
+      case (.scissors, .lose): return .paper
+      case (.scissors, .win): return .rock
+      case (_, .draw): return self
+    }
+  }
 }
 
 public func partTwo() {
-
+  let finalScore = input.split(separator: "\n").map {
+    let outcome = Outcome(rawValue: $0.last!)!
+    let play = Play(rawValue: $0.first!)!.playForOutcome(outcome)
+    return outcome.score + play.score
+  }.reduce(0, +)
+  print(finalScore)
 }
--- Sources/Day3/Day3.swift
+++ Sources/Day3/Day3.swift
@@ -1,9 +1,42 @@
 import Algorithms
+import AdventUtilities
+
+func priority(of char: Character) -> Int {
+  switch char {
+    case "a"..."z":
+      return Int(char.asciiValue! - Character("a").asciiValue!) + 1
+    case "A"..."Z":
+      return Int(char.asciiValue! - Character("A").asciiValue!) + 27
+    default:
+      return 0
+  }
+}
 
 public func partOne() {
+  let result = input.split(separator: "\n").map {
+    let front = Set($0.prefix($0.count / 2))
+    let back = Set($0.suffix($0.count / 2))
+    let char = front.intersection(back).only!
+    return priority(of: char)
+  }.reduce(0, +)
+  print(result)
+}
 
+func priorityOfGroup(_ group: any Collection<[Character]>) -> Int {
+  let group = group.map(Set.init)
+  guard let first = group.first else { return 0 }
+  let char = group.reduce(first) { result, value in
+    result.intersection(value)
+  }.only!
+  return priority(of: char)
 }
 
 public func partTwo() {
-
+  let result = input
+    .split(separator: "\n")
+    .map(Array.init)
+    .chunks(ofCount: 3)
+    .map(priorityOfGroup)
+    .reduce(0, +)
+  print(result) // 2760
 }
